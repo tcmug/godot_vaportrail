1dd3bb2ef25ebc74240be513a812d3988513c429
Maija Lintunokka <maija.lintunokka@gmail.com>
2026-04-06 20:30:59 +0300

    feat: add godot docs

--- src/vapor_trail_bindings.cpp
+++ src/vapor_trail_bindings.cpp
@@ -43,66 +43,45 @@ void VaporTrail::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_alignment"), &VaporTrail::get_alignment);
 	ClassDB::bind_method(D_METHOD("get_mesh_node"), &VaporTrail::get_mesh_node);
 
-	ADD_GROUP("Config", "config");
-	ADD_GROUP("Visual", "visual");
-
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::INT, "config/alignment", PROPERTY_HINT_ENUM, "Camera,X,Y,Z"), "set_alignment", "get_alignment");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::NODE_PATH, "config/geometry_nodepath"), "set_geometry_root", "get_geometry_root");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::INT, "config/num_points", PROPERTY_HINT_RANGE, "2,10000,1"), "set_num_points", "get_num_points");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "config/update_interval"), "set_update_interval", "get_update_interval");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "config/randomness"), "set_randomness", "get_randomness");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::OBJECT, "visual/material", PROPERTY_HINT_RESOURCE_TYPE, "Material"), "set_material", "get_material");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "visual/size", PROPERTY_HINT_RANGE, "0.0,100.0,0.01"), "set_size", "get_size");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::OBJECT, "visual/size_curve", PROPERTY_HINT_RESOURCE_TYPE, "Curve"), "set_curve", "get_curve");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::OBJECT, "visual/time_curve", PROPERTY_HINT_RESOURCE_TYPE, "Curve"), "set_time_curve", "get_time_curve");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::OBJECT, "visual/time_color_gradient", PROPERTY_HINT_RESOURCE_TYPE, "Gradient"), "set_time_color_gradient", "get_time_color_gradient");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "visual/current_time", PROPERTY_HINT_RANGE, "0.0,1.0,0.01"), "set_current_time", "get_current_time");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "visual/minimum_onscreen_size", PROPERTY_HINT_RANGE, "0.0,100.0,0.01"), "set_minimum_onscreen_size", "get_minimum_onscreen_size");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::OBJECT, "visual/color_gradient", PROPERTY_HINT_RESOURCE_TYPE, "Gradient"), "set_gradient", "get_gradient");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "visual/uv_shift", PROPERTY_HINT_RANGE, "-100.0,100.0,0.001"), "set_uv_shift", "get_uv_shift");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::INT, "visual/uv_alignment", PROPERTY_HINT_ENUM, "Fixed,Rolling"), "set_uv_alignment", "get_uv_alignment");
+	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::BOOL, "emitting"), "set_emitting", "get_emitting");
+	// ClassDB::add_property("VaporTrail", PropertyInfo(Variant::INT, "config/alignment", PROPERTY_HINT_ENUM, "Camera,X,Y,Z"), "set_alignment", "get_alignment");
+	// ClassDB::add_property("VaporTrail", PropertyInfo(Variant::NODE_PATH, "config/geometry_nodepath"), "set_geometry_root", "get_geometry_root");
+	// ClassDB::add_property("VaporTrail", PropertyInfo(Variant::INT, "config/num_points", PROPERTY_HINT_RANGE, "2,10000,1"), "set_num_points", "get_num_points");
+	// ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "config/update_interval"), "set_update_interval", "get_update_interval");
+	// ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "config/randomness"), "set_randomness", "get_randomness");
+	// ClassDB::add_property("VaporTrail", PropertyInfo(Variant::OBJECT, "visual/material", PROPERTY_HINT_RESOURCE_TYPE, "Material"), "set_material", "get_material");
+	// ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "visual/size", PROPERTY_HINT_RANGE, "0.0,100.0,0.01"), "set_size", "get_size");
+	// ClassDB::add_property("VaporTrail", PropertyInfo(Variant::OBJECT, "visual/size_curve", PROPERTY_HINT_RESOURCE_TYPE, "Curve"), "set_curve", "get_curve");
+	// ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "visual/minimum_onscreen_size", PROPERTY_HINT_RANGE, "0.0,100.0,0.01"), "set_minimum_onscreen_size", "get_minimum_onscreen_size");
+	// ClassDB::add_property("VaporTrail", PropertyInfo(Variant::OBJECT, "visual/color_gradient", PROPERTY_HINT_RESOURCE_TYPE, "Gradient"), "set_gradient", "get_gradient");
+	// ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "visual/uv_shift", PROPERTY_HINT_RANGE, "-100.0,100.0,0.001"), "set_uv_shift", "get_uv_shift");
+	// ClassDB::add_property("VaporTrail", PropertyInfo(Variant::INT, "visual/uv_alignment", PROPERTY_HINT_ENUM, "Fixed,Rolling"), "set_uv_alignment", "get_uv_alignment");
 
 	// Runtime-accessible flat properties (hidden from editor)
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::BOOL, "emitting"), "set_emitting", "get_emitting");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::INT, "alignment", PROPERTY_HINT_ENUM, "Camera,X,Y,Z", PROPERTY_USAGE_NO_EDITOR), "set_alignment", "get_alignment");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::NODE_PATH, "geometry_nodepath", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_NO_EDITOR), "set_geometry_root", "get_geometry_root");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::INT, "num_points", PROPERTY_HINT_RANGE, "2,10000,1", PROPERTY_USAGE_NO_EDITOR), "set_num_points", "get_num_points");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "update_interval", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_NO_EDITOR), "set_update_interval", "get_update_interval");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "randomness", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_NO_EDITOR), "set_randomness", "get_randomness");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::OBJECT, "material", PROPERTY_HINT_RESOURCE_TYPE, "Material", PROPERTY_USAGE_NO_EDITOR), "set_material", "get_material");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "size", PROPERTY_HINT_RANGE, "0.0,100.0,0.01", PROPERTY_USAGE_NO_EDITOR), "set_size", "get_size");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::OBJECT, "size_curve", PROPERTY_HINT_RESOURCE_TYPE, "Curve", PROPERTY_USAGE_NO_EDITOR), "set_curve", "get_curve");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::OBJECT, "time_curve", PROPERTY_HINT_RESOURCE_TYPE, "Curve", PROPERTY_USAGE_NO_EDITOR), "set_time_curve", "get_time_curve");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::OBJECT, "time_color_gradient", PROPERTY_HINT_RESOURCE_TYPE, "Gradient", PROPERTY_USAGE_NO_EDITOR), "set_time_color_gradient", "get_time_color_gradient");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "current_time", PROPERTY_HINT_RANGE, "0.0,1.0,0.01", PROPERTY_USAGE_NO_EDITOR), "set_current_time", "get_current_time");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "minimum_onscreen_size", PROPERTY_HINT_RANGE, "0.0,100.0,0.01", PROPERTY_USAGE_NO_EDITOR), "set_minimum_onscreen_size", "get_minimum_onscreen_size");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::OBJECT, "color_gradient", PROPERTY_HINT_RESOURCE_TYPE, "Gradient", PROPERTY_USAGE_NO_EDITOR), "set_gradient", "get_gradient");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "uv_shift", PROPERTY_HINT_RANGE, "-100.0,100.0,0.001", PROPERTY_USAGE_NO_EDITOR), "set_uv_shift", "get_uv_shift");
-	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::INT, "uv_alignment", PROPERTY_HINT_ENUM, "Fixed,Rolling", PROPERTY_USAGE_NO_EDITOR), "set_uv_alignment", "get_uv_alignment");
+	ADD_GROUP("Config", "");
+	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::INT, "alignment", PROPERTY_HINT_ENUM, "Camera,X,Y,Z"), "set_alignment", "get_alignment");
+	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::NODE_PATH, "geometry_nodepath", PROPERTY_HINT_NONE, ""), "set_geometry_root", "get_geometry_root");
+	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::INT, "num_points", PROPERTY_HINT_RANGE, "2,10000,1"), "set_num_points", "get_num_points");
+	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "update_interval", PROPERTY_HINT_NONE, ""), "set_update_interval", "get_update_interval");
+	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "randomness", PROPERTY_HINT_NONE, ""), "set_randomness", "get_randomness");
+
+	ADD_GROUP("Visual", "");
+	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::OBJECT, "material", PROPERTY_HINT_RESOURCE_TYPE, "Material"), "set_material", "get_material");
+	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "size", PROPERTY_HINT_RANGE, "0.0,100.0,0.01"), "set_size", "get_size");
+	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::OBJECT, "size_curve", PROPERTY_HINT_RESOURCE_TYPE, "Curve"), "set_curve", "get_curve");
+	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "minimum_onscreen_size", PROPERTY_HINT_RANGE, "0.0,100.0,0.01"), "set_minimum_onscreen_size", "get_minimum_onscreen_size");
+	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::OBJECT, "color_gradient", PROPERTY_HINT_RESOURCE_TYPE, "Gradient"), "set_gradient", "get_gradient");
+	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "uv_shift", PROPERTY_HINT_RANGE, "-100.0,100.0,0.001"), "set_uv_shift", "get_uv_shift");
+	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::INT, "uv_alignment", PROPERTY_HINT_ENUM, "Fixed,Rolling"), "set_uv_alignment", "get_uv_alignment");
+
+	ADD_GROUP("Temporal", "");
+	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::OBJECT, "time_curve", PROPERTY_HINT_RESOURCE_TYPE, "Curve"), "set_time_curve", "get_time_curve");
+	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::OBJECT, "time_color_gradient", PROPERTY_HINT_RESOURCE_TYPE, "Gradient"), "set_time_color_gradient", "get_time_color_gradient");
+	ClassDB::add_property("VaporTrail", PropertyInfo(Variant::FLOAT, "current_time", PROPERTY_HINT_RANGE, "0.0,1.0,0.01"), "set_current_time", "get_current_time");
 }
 
 bool VaporTrail::_set(const StringName &p_name, const Variant &p_value) {
-	// Migration from old config to new.
-	if (p_name == StringName("num_points")) {
-		set_num_points(p_value);
-		return true;
-	}
-	if (p_name == StringName("size")) {
-		set_size(p_value);
-		return true;
-	}
-	if (p_name == StringName("geometry_nodepath")) {
-		set_geometry_root(p_value);
-		return true;
-	}
-	if (p_name == StringName("update_interval")) {
-		set_update_interval(p_value);
-		return true;
-	}
-	if (p_name == StringName("material")) {
-		set_material(p_value);
-		return true;
-	}
+	// Migration from old property names.
 	if (p_name == StringName("curve")) {
 		set_curve(p_value);
 		return true;
@@ -115,11 +94,52 @@ bool VaporTrail::_set(const StringName &p_name, const Variant &p_value) {
 		set_randomness(p_value);
 		return true;
 	}
-	if (p_name == StringName("uv_shift")) {
+	// Migration from old config/visual/temporal prefixed names.
+	if (p_name == StringName("config/alignment")) {
+		set_alignment(p_value);
+		return true;
+	}
+	if (p_name == StringName("config/geometry_nodepath")) {
+		set_geometry_root(p_value);
+		return true;
+	}
+	if (p_name == StringName("config/num_points")) {
+		set_num_points(p_value);
+		return true;
+	}
+	if (p_name == StringName("config/update_interval")) {
+		set_update_interval(p_value);
+		return true;
+	}
+	if (p_name == StringName("config/randomness")) {
+		set_randomness(p_value);
+		return true;
+	}
+	if (p_name == StringName("visual/material")) {
+		set_material(p_value);
+		return true;
+	}
+	if (p_name == StringName("visual/size")) {
+		set_size(p_value);
+		return true;
+	}
+	if (p_name == StringName("visual/size_curve")) {
+		set_curve(p_value);
+		return true;
+	}
+	if (p_name == StringName("visual/minimum_onscreen_size")) {
+		set_minimum_onscreen_size(p_value);
+		return true;
+	}
+	if (p_name == StringName("visual/color_gradient")) {
+		set_gradient(p_value);
+		return true;
+	}
+	if (p_name == StringName("visual/uv_shift")) {
 		set_uv_shift(p_value);
 		return true;
 	}
-	if (p_name == StringName("uv_alignment")) {
+	if (p_name == StringName("visual/uv_alignment")) {
 		set_uv_alignment(p_value);
 		return true;
 	}
